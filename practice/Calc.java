c79e667b1372f2f00d5a6d2fa890b294a25f01f5
Test <>
2022-08-25 16:16:56 +0900

    Test

---
+++ practice/Calc.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class Calc {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 2;
+		int total = CalcLogic.tasu(a, b);
+		int delta = CalcLogic.hiku(a, b);
+		System.out.println("たすと" + total + "、ひくと" + delta);
+
+
+
+	}
+}
---
+++ practice/Main.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class Main {
+	public static void main(String[] args) {
+
+
+
+		
+		
+		
+		
+
+	}
+}
