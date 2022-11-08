bd42f9ddf3fbdb4bff6b59fcbb19169cff43c295
suzu784 <s.suzuki784@hotmail.com>
2022-11-08 22:27:14 +0900

    三角形を描く

---
+++ src/algorithm/TextTriangle1.java
@@ -0,0 +1,16 @@
+package algorithm;
+
+public class TextTriangle1 {
+
+	public static void main(String[] args) {
+		for(int i = 0; i < 10; i++) {
+			// 1行の出力
+			for(int j = 0; j < i; j++) {
+				System.out.print("●");
+			}
+			System.out.println("");
+		}
+
+	}
+
+}
