a1279f085a5b3546464934f18cf65ca45ce8ccd5
suzu784 <s.suzuki784@hotmail.com>
2022-11-09 17:42:40 +0900

    横ストライプ

---
+++ src/algorithm/Q007.java
@@ -0,0 +1,21 @@
+package algorithm;
+
+public class Q007 {
+
+	public static void main(String[] args) {
+		int row = 5;
+		int col = 7;
+		
+		for(int i = 0; i < row; i++) {
+			if(i % 2 == 0) {
+				for(int j = 0; j < col; j++) {
+					System.out.print("■");
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+}
