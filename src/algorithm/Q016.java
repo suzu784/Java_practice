4e98cd107c0e068162d535916383f54f54075d37
suzu784 <s.suzuki784@hotmail.com>
2022-11-09 23:41:43 +0900

    1から指定の数までの指定倍の数を縦に表示する

---
+++ src/algorithm/Q016.java
@@ -0,0 +1,16 @@
+package algorithm;
+
+public class Q016 {
+
+	public static void main(String[] args) {
+		int n = 7;
+		int bai = 5;
+		
+		for(int i = 1; i <= n; i++) {
+			int x = i * bai;
+			System.out.println(x);
+		}
+
+	}
+
+}
