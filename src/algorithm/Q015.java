54b720ee54b0a8fb9da31862b76c192e3cf5d991
suzu784 <s.suzuki784@hotmail.com>
2022-11-09 23:36:36 +0900

    1から指定の数までの10倍の数を縦に表示する

---
+++ src/algorithm/Q015.java
@@ -0,0 +1,15 @@
+package algorithm;
+
+public class Q015 {
+
+	public static void main(String[] args) {
+		int n = 7;
+		
+		for(int i = 1; i <= n; i++) {
+			int x = i * 10;
+			System.out.println(x);
+		}
+
+	}
+
+}
