ffc31d8b99de8912d931df77d632765948a280d3
suzu784 <s.suzuki784@hotmail.com>
2022-11-10 16:46:31 +0900

    1から指定の数までカンマ区切りで表示する

---
+++ src/algorithm/Q020.java
@@ -0,0 +1,17 @@
+package algorithm;
+
+public class Q020 {
+
+	public static void main(String[] args) {
+		int n = 7;
+		
+		for(int i = 1; i <= n; i++) {
+			System.out.print(i);
+			if(i != n) {
+				System.out.print(",");
+			}
+		}
+
+	}
+
+}
