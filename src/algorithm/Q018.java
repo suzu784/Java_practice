dbda544fec68a5054c021946461cf0c68ab1be62
suzu784 <s.suzuki784@hotmail.com>
2022-11-10 16:39:02 +0900

    指定の数から1まで縦に表示する

---
+++ src/algorithm/Q018.java
@@ -0,0 +1,14 @@
+package algorithm;
+
+public class Q018 {
+
+	public static void main(String[] args) {
+		int n = 10;
+		
+		for(int i = n; i >= 1; i--) {
+			System.out.println(i);
+		}
+
+	}
+
+}
