6922ef06a10acaa2723bba702a1ba32cadbb6017
suzu784 <s.suzuki784@hotmail.com>
2022-11-11 18:44:01 +0900

    キャラクターの名前

---
+++ src/paiza/D217.java
@@ -0,0 +1,14 @@
+package paiza;
+
+import java.util.Scanner;
+
+public class D217 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("好きな文字を入力してください");
+		String line = sc.nextLine();
+		System.out.println(line.length());
+	}
+
+}
