8063286ee3967f270abe3380177f15efa3bf1316
suzu784 <s.suzuki784@hotmail.com>
2022-11-11 23:58:44 +0900

    しりとりの判定

---
+++ src/paiza/C114.java
@@ -0,0 +1,30 @@
+package paiza;
+
+import java.util.Scanner;
+
+public class C114 {
+
+	public static void main(String[] args) {
+		// 判定文がいる(末尾と先頭が一致しているか)
+		// 最初に数字を入力し、その回数文ループにて回す
+		Scanner sc = new Scanner(System.in);
+		System.out.println("数字を入力してください");
+        int line = sc.nextInt();
+        
+        for(int i = 1; i < line; i++) {
+            Scanner scn = new Scanner(System.in);
+            String a = scn.nextLine();
+            String b = scn.nextLine();
+            String aLast = a.substring(a.length() - 1);
+            String bFirst = b.substring(0, 1);
+            
+            if(!aLast.equals(bFirst)) {
+                System.out.print(aLast);
+                System.out.print(" ");
+                System.out.print(bFirst);
+                break;
+            }
+        }
+	}
+
+}
