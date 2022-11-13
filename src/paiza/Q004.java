ec9ab68c298ad16335b089acb49b7afdc90eca9e
suzu784 <s.suzuki784@hotmail.com>
2022-11-13 15:10:54 +0900

    要素を取得

---
+++ src/paiza/Q004.java
@@ -0,0 +1,13 @@
+package paiza;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Q004 {
+
+	public static void main(String[] args) {
+		ArrayList<String> sample_list = new ArrayList<>(Arrays.asList("赤", "青", "緑"));
+		System.out.println(sample_list.get(2));
+	}
+
+}
