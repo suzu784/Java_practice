a55a3ec63b2c4b0b57774c70fa1280bab0198d30
suzu784 <s.suzuki784@hotmail.com>
2022-11-15 11:22:27 +0900

    累乗した値を取得する

---
+++ src/JSample4_6.java
@@ -0,0 +1,18 @@
+
+public class JSample4_6 {
+	
+	// Math.pow 累乗した値を取得する
+
+	public static void main(String[] args) {
+		double d = 3.0;
+		
+		System.out.println(Math.pow(d, 0.0));
+		System.out.println(Math.pow(d, 1.0));
+		System.out.println(Math.pow(d, 2.0));
+		System.out.println(Math.pow(d, 3.0));
+		System.out.println(Math.pow(d, 4.0));
+		System.out.println(Math.pow(d, 5.0));
+
+	}
+
+}
