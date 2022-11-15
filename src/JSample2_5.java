e6de7cb095f56a15752cfebda08ada10b3392768
suzu784 <s.suzuki784@hotmail.com>
2022-11-15 11:18:48 +0900

    どちらか大きい値または小さい値を取得する

---
+++ src/JSample2_5.java
@@ -0,0 +1,13 @@
+
+public class JSample2_5 {
+
+	public static void main(String[] args) {
+		int num1 = 20;
+		int num2 = 12;
+		
+		System.out.println(Math.max(num1, num2));
+		System.out.println(Math.min(num1, num2));
+
+	}
+
+}
