2f0b936cd730c4c5c474efeff2110dd8b9d45e7c
suzu784 <s.suzuki784@hotmail.com>
2022-11-26 11:44:43 +0900

    コンストラクター

---
+++ src/algorithm2/Java2_8.java
@@ -0,0 +1,20 @@
+package algorithm2;
+
+public class Java2_8 {
+
+	public static void main(String[] args) {
+		class YMD {
+			int y;
+			int m;
+			int d;
+			
+			YMD(int y, int m, int d) {
+				this.y = y;
+				this.m = m;
+				this.d = d;
+			}
+		}
+
+	}
+
+}
