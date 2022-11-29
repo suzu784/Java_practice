167a9edff8d8de2926bcbb002291bcbca50706f5
suzu784 <s.suzuki784@hotmail.com>
2022-11-29 22:35:49 +0900

    フィボナッチ数列

---
+++ src/Fibonacchi.java
@@ -0,0 +1,23 @@
+
+public class Fibonacchi {
+	
+	// フィボナッチ数列
+
+	public static void main(String[] args) {
+		int f0, f1, fn;
+		
+		f0 = 0;
+		System.out.println(f0);
+		
+		f1 = 1;
+		System.out.println(f1);
+	
+		for(int i = 2; i <= 10; i++) {
+			fn = f0 + f1;
+			System.out.println(fn);
+			f0 = f1;
+			f1 = fn;
+		}
+	}
+
+}
