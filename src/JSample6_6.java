7af2c4731853956b0124f08060eeed5eb2cfab39
suzu784 <s.suzuki784@hotmail.com>
2022-12-09 19:14:27 +0900

    スキャナの入力ストリームにまだ値が残っているか確認する

---
+++ src/JSample6_6.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class JSample6_6 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner("AB BC CA");
+		
+		while(sc.hasNext()) {
+			System.out.println(sc.next());
+		}
+
+	}
+
+}
